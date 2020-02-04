4aed000aad13ebb3c13b83bf07cbd510ea0726c4
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-04 19:32:44 +0300

    Add ToyComparator test

---
+++ src/test/java/by/kiselevich/task1/comparator/ToyComparatorByCostReversedThenSizeReversedTest.java
@@ -0,0 +1,31 @@
+package by.kiselevich.task1.comparator;
+
+import by.kiselevich.task1.entity.toy.Ball;
+import by.kiselevich.task1.entity.toy.Toy;
+import by.kiselevich.task1.entity.toy.ToySize;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+
+public class ToyComparatorByCostReversedThenSizeReversedTest extends Assert {
+
+    @Test
+    public void compareToysByCostTest() {
+        Comparator<Toy> comparator = new ToyComparatorByCostReversedThenSizeReversed();
+        Toy toy1 = new Ball(ToySize.SMALL, BigDecimal.valueOf(10));
+        Toy toy2 = new Ball(ToySize.SMALL, BigDecimal.valueOf(20));
+        assertTrue(comparator.compare(toy1, toy2) > 0);
+        assertTrue(comparator.compare(toy2, toy1) < 0);
+    }
+
+    @Test
+    public void compareToysBySizeTest() {
+        Comparator<Toy> comparator = new ToyComparatorByCostReversedThenSizeReversed();
+        Toy toy1 = new Ball(ToySize.SMALL, BigDecimal.valueOf(10));
+        Toy toy2 = new Ball(ToySize.BIG, BigDecimal.valueOf(10));
+        assertTrue(comparator.compare(toy1, toy2) > 0);
+        assertTrue(comparator.compare(toy2, toy1) < 0);
+    }
+}

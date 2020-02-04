75dfb2687f926e16a2f60d51b6297d2b862d7101
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-04 15:18:26 +0300

    Fix children initiation in controller

--- src/main/java/by/kiselevich/taskOOP/controller/ChildRoomController.java
+++ src/main/java/by/kiselevich/taskOOP/controller/ChildRoomController.java
@@ -9,7 +9,12 @@ import by.kiselevich.taskOOP.service.ChildRoomService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 public class ChildRoomController {
 
@@ -28,8 +33,19 @@ public class ChildRoomController {
     }
 
     private void initChildren() {
-        childRepository.addChildren(new ChildFileReader(CHILDREN_FILEPATH).readChildrenFromFile());
-        LOGGER.info("Children initiated");
+        Optional<URL> optionalURL = Optional.ofNullable(
+                this.getClass().getClassLoader().getResource(CHILDREN_FILEPATH));
+        if (optionalURL.isPresent()) {
+            try {
+                String path = Paths.get(optionalURL.get().toURI()).toString();
+                childRepository.addChildren(new ChildFileReader(path).readChildrenFromFile());
+                LOGGER.info("Children initiated");
+            } catch (URISyntaxException e) {
+                LOGGER.warn(e);
+            }
+        } else {
+            LOGGER.warn("Children file not found: " + CHILDREN_FILEPATH);
+        }
     }
 
     public void serveChildren() {

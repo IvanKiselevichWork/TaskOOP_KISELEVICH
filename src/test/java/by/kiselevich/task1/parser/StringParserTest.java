90d6a43d9458c9087500593ce9fb270dd250f872
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-04 19:48:24 +0300

    Add StringParser tests

---
+++ src/test/java/by/kiselevich/task1/parser/StringParserTest.java
@@ -0,0 +1,41 @@
+package by.kiselevich.task1.parser;
+
+import by.kiselevich.task1.exception.ChildParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.InputMismatchException;
+
+public class StringParserTest extends Assert {
+
+    @Test
+    public void parseStringToBigDecimalTestPositive() throws ChildParseException {
+        String string = "234523";
+        StringParser stringParser = new StringParser();
+        BigDecimal bigDecimal = stringParser.parseStringToBigDecimal(string);
+        assertEquals(bigDecimal, BigDecimal.valueOf(234523));
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void parseStringToBigDecimalTestNegative() throws ChildParseException {
+        String string = "asdfasdf";
+        StringParser stringParser = new StringParser();
+        stringParser.parseStringToBigDecimal(string);
+    }
+
+    @Test
+    public void parseStringToIntTestPositive() throws ChildParseException {
+        String string = "234523";
+        StringParser stringParser = new StringParser();
+        int num = stringParser.parseStringToInt(string);
+        assertEquals(num, 234523);
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void parseStringToIntTestNegative() throws ChildParseException {
+        String string = "asdfasdf";
+        StringParser stringParser = new StringParser();
+        stringParser.parseStringToInt(string);
+    }
+}

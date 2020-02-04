243bc810fe552422e806483355e5f88a8b0a68b2
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-04 15:30:42 +0300

    Add exception message

--- src/main/java/by/kiselevich/taskOOP/exception/ToyExceptionMessages.java
+++ src/main/java/by/kiselevich/taskOOP/exception/ToyExceptionMessages.java
@@ -6,7 +6,8 @@ public enum ToyExceptionMessages {
     INVALID_TOY_TYPE("Invalid toy type"),
     INVALID_TOY_SIZE("Invalid toy size"),
     INVALID_TOY_COST("Invalid toy cost"),
-    INVALID_TOY_COUNT("Invalid toy count");
+    INVALID_TOY_COUNT("Invalid toy count"),
+    TOY_NOT_PASS_VALIDATION("Toy not pass validation");
 
     private String message;
 

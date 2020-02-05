1e6cd46becce5d4fc8606f43de94f73447e539de
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-05 09:48:35 +0300

    Add StringValidator tests

---
+++ src/test/java/by/kiselevich/task1/validator/StringValidatorTest.java
@@ -0,0 +1,48 @@
+package by.kiselevich.task1.validator;
+
+import by.kiselevich.task1.entity.toy.ToySize;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+public class StringValidatorTest extends Assert {
+
+    StringValidator stringValidator = new StringValidator();
+
+    @Test
+    public void isStringParsableToBigDecimalTestPositive() {
+        String string = "34";
+        assertTrue(stringValidator.isStringParsableToBigDecimal(string));
+    }
+
+    @Test
+    public void isStringParsableToBigDecimalTestNegative() {
+        String string = "qwqw";
+        assertFalse(stringValidator.isStringParsableToBigDecimal(string));
+    }
+
+    @Test
+    public void isStringParsableToIntTestPositive() {
+        String string = "34";
+        assertTrue(stringValidator.isStringParsableToInt(string));
+    }
+
+    @Test
+    public void isStringParsableToIntTestNegative() {
+        String string = "qwqw";
+        assertFalse(stringValidator.isStringParsableToInt(string));
+    }
+
+    @Test
+    public void isStringParsableToEnumTestPositive() {
+        String string = "BIG";
+        assertTrue(stringValidator.isStringParsableToEnum(ToySize.class, string));
+    }
+
+    @Test
+    public void isStringParsableToEnumTestNegative() {
+        String string = "BIG1212";
+        assertFalse(stringValidator.isStringParsableToEnum(ToySize.class, string));
+    }
+}

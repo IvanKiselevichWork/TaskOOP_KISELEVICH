54f58234b6099a27fb735f40104d31c58aaa9a29
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-07 09:28:00 +0300

    Add GetAllToysSortedByCost toy repository specification

---
+++ src/main/java/by/kiselevich/task1/specification/toy/GetAllToysSortedByCost.java
@@ -0,0 +1,18 @@
+package by.kiselevich.task1.specification.toy;
+
+import by.kiselevich.task1.comparator.ToyComparatorByCost;
+import by.kiselevich.task1.comparator.ToyComparatorByCostReversedThenSizeReversed;
+import by.kiselevich.task1.entity.toy.Toy;
+import by.kiselevich.task1.repository.Repository;
+
+import java.util.List;
+
+public class GetAllToysSortedByCost implements ToySpecification {
+
+    @Override
+    public List<Toy> query(Repository<Toy> repository) {
+        List<Toy> result = repository.getAll();
+        result.sort(new ToyComparatorByCost());
+        return result;
+    }
+}

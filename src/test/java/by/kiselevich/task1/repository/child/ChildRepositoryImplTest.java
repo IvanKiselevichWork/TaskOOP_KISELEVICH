fd4f864beb756d6ae323ee76ae2eff0bece1af98
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-07 15:06:34 +0300

    Add tests to ChildRepositoryImplTest

--- src/test/java/by/kiselevich/task1/repository/child/ChildRepositoryImplTest.java
+++ src/test/java/by/kiselevich/task1/repository/child/ChildRepositoryImplTest.java
@@ -24,7 +24,7 @@ public class ChildRepositoryImplTest extends Assert {
     }
 
     @Test
-    public void addTest() {
+    public void addTest1() {
         Child child1 = new Child();
         child1.setLastName("Gdfsdf");
         ChildRepository childRepository = new ChildRepositoryImpl();
@@ -35,7 +35,19 @@ public class ChildRepositoryImplTest extends Assert {
     }
 
     @Test
-    public void removeTest() {
+    public void addTest2() {
+        Child child1 = new Child();
+        child1.setLastName("Gdfsdf");
+        ChildRepository childRepository = new ChildRepositoryImpl();
+        childRepository.add(child1);
+        childRepository.add(child1);
+        List<Child> children = childRepository.getAll();
+        assertEquals(1, children.size());
+        assertTrue(children.contains(child1));
+    }
+
+    @Test
+    public void removeTest1() {
         Child child1 = new Child();
         child1.setLastName("Gdfsdf");
         Child child2 = new Child();
@@ -50,7 +62,25 @@ public class ChildRepositoryImplTest extends Assert {
     }
 
     @Test
-    public void updateTest() {
+    public void removeTest2() {
+        Child child1 = new Child();
+        child1.setLastName("Gdfsdf");
+        Child child2 = new Child();
+        child2.setLastName("Zdfsdf");
+        ChildRepository childRepository = new ChildRepositoryImpl();
+        childRepository.add(child1);
+        childRepository.add(child2);
+        Child child3 = new Child();
+        child2.setLastName("Zdfsdf");
+        childRepository.remove(child3);
+        List<Child> children = childRepository.getAll();
+        assertEquals(2, children.size());
+        assertTrue(children.contains(child1));
+        assertTrue(children.contains(child2));
+    }
+
+    @Test
+    public void updateTest1() {
         Child child1 = new Child();
         child1.setLastName("Gdfsdf");
         ChildRepository childRepository = new ChildRepositoryImpl();
@@ -60,5 +90,15 @@ public class ChildRepositoryImplTest extends Assert {
         List<Child> children = childRepository.getAll();
         assertEquals(1, children.size());
         assertTrue(children.contains(child1));
+    }
+
+    @Test
+    public void updateTest2() {
+        Child child1 = new Child();
+        child1.setLastName("Gdfsdf");
+        ChildRepository childRepository = new ChildRepositoryImpl();
+        childRepository.update(child1);
+        List<Child> children = childRepository.getAll();
+        assertEquals(0, children.size());
     }
 }
